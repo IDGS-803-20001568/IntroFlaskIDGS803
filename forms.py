0a1d78bfa97b9769caddcd9d4e5783ff0364fbb7
IDGS-803-20001568 <cristianglr95@gmail.com>
2024-02-13 18:44:50 -0600

    CGLR 13/02/2024 lectura de archivos

--- forms.py
+++ forms.py
@@ -1,10 +1,23 @@
 from wtforms import Form
-from wtforms import StringField,SelectField,RadioField,EmailField,IntegerField
+from wtforms import StringField, SelectField, RadioField, EmailField, IntegerField
+from wtforms import validators
 
-class UserForm(Form):
-    
-    nombre=StringField('nombre')
-    apaterno=StringField('apaterno')
-    amaterno=StringField('amaterno')
-    edad=IntegerField('edad')
-    correo=EmailField('email')    
+class UsersForm(Form):
+    nombre=StringField('nombre', [
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=10, message='Ingresa nombre valido')
+    ])
+    apaterno=StringField('apaterno', [
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=10, message='Ingresa apellido paterno valido')
+    ])
+    amaterno=StringField('amaterno', [
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=10, message='Ingresa apellido materno valido')
+    ])
+    edad=IntegerField('edad', [
+        validators.number_range(min=1, max=20, message='Valor no valido')
+    ])
+    correo=EmailField('correo', [
+        validators.Email(message='Ingrese un correo valido')
+    ])
